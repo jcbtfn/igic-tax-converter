11d9b80142c1063d78e26f03b7733bdc0af22abf
jacobtfn <jacob.tfn@gmail.com>
2023-12-11 12:51:28 +0000

    tests and types of taxes added

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,12 +1,25 @@
 const tax_conversion_test = require('../src/index');
 
-test('return 0 if the amount is 0, independently of tax values', () => {
-    expect(tax_conversion_test(0, "IVA", "IGIC")).toBe(0);
-    expect(tax_conversion_test(0, "IGIC", "IVA")).toBe(0);
+describe("returns 0 if the input is 0", () => {
+    test("returns 0 if the amount is 0, input tax is IVA and output tax is IGIC", () => {
+        expect(tax_conversion_test(0, "IVA", "IGIC")).toBe(0);
+    });
+    test("returns 0 if the amount is 0, input tax is IGIC and output tax is IVA", () => {
+        expect(tax_conversion_test(0, "IGIC", "IVA")).toBe(0);
+    });
+    test("returns 0 if the amount is 0, input tax is NONE and output tax is NONE", () => {
+        expect(tax_conversion_test(0, "NONE", "NONE")).toBe(0);
+    });
 });
 
-test('return the same amount if tax values are the same', () => {
-    expect(tax_conversion_test(0, "IVA", "IVA")).toBe(0);
-    expect(tax_conversion_test(50, "IGIC", "IGIC")).toBe(50);
-    expect(tax_conversion_test(999, "NONE", "NONE")).toBe(999);
-});
+describe("returns the same input amount if tax values are the same", () => {
+    test("returns 1234 if the input is 1234 and input tax and output tax are IVA", () => {
+        expect(tax_conversion_test(1234, "IVA", "IVA")).toBe(1234);
+    });
+    test("returns 50 if the input is 50 and input tax and output tax are IGIC", () => {
+        expect(tax_conversion_test(50, "IGIC", "IGIC")).toBe(50);
+    });
+    test("returns 999 if the input is 999 and input tax and output tax are NONE", () => {
+        expect(tax_conversion_test(999, "NONE", "NONE")).toBe(999);
+    });
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+const taxesList: Record<string, number> = {
+    "NONE": 0,
+    "IVA": 21,
+    "IVA_REDUCIDO": 10,
+    "IVA_SUPERREDUCIDO": 4,
+    "IVA_EXCENTO": 0,
+    "IGIC_ESPECIAL_INCREMENTADO": 13.5, 
+    "IGIC_INCREMENTADO": 9.5,
+    "IGIC_GENERAL": 7,
+    "IGIC_REDUCIDO": 3,
+    "IGIC_TIPO_CERO": 0,
+}
+
 function tax_conversion (amount_to_convert: number, input_tax: string, output_tax: string): number {
     if (amount_to_convert === 0) {
         return 0;
@@ -10,7 +23,7 @@ function tax_conversion (amount_to_convert: number, input_tax: string, output_ta
 }
 
 function tax_strip (amount_to_strip: number, tax_to_strip: string): number{
-    return 0;
+    return (amount_to_strip - amount_to_strip*0);
 };
 
 module.exports = tax_conversion;
